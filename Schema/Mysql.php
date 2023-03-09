b8c6e21ce9b012cef0ba79c182b726589f186441
Shaun-Fong <ghost_h@foxmail.com>
2023-03-09 22:43:11 +0800

    fix(#5): fix MySQL schema faulty. [Link](https://kanboard.discourse.group/t/issues-with-plugins/2435/17?u=aljawaid)

--- Schema/Mysql.php
+++ Schema/Mysql.php
@@ -8,5 +8,5 @@ const VERSION = 1;
 
 function version_1(PDO $pdo)
 {
-    $pdo->exec("ALTER TABLE `subtasks` ADD COLUMN `due_description` TEXT NOT NULL DEFAULT ''");
+    $pdo->exec("ALTER TABLE `subtasks` ADD COLUMN `due_description` TEXT NOT NULL ");
 }
